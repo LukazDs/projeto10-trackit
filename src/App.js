46d1d8badcf5c1c9b423722056f7008c8987f3c6
Lucas Duarte <lukazds2016@gmail.com>
2022-05-26 21:49:34 -0300

    Tela 1 estática montada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./assets/styles/reset.css"
 import "./assets/styles/styles.css"
-import logo from "./assets/images/logo.svg"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage"
+import SignPage from "./components/SignPage";
+
 
 function App() {
-  return (<span>Aplicação</span>);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cadastro" element={<SignPage />} />
+      </Routes>
+    </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from "../assets/images/logo.svg"
+
+export default function LoginPage() {
+
+    return (
+
+        <Container>
+            <img src={logo} />
+            <Forms>
+                <input placeholder='email'></input>
+                <input placeholder='senha'></input>
+                <button>Entrar</button>
+            </Forms>
+            <a href=''>Não tem uma conta? Cadastre-se!</a>
+        </Container>
+
+
+    )
+}
+
+const Container = styled.div`
+    width: 375px;
+    height: 667px;
+    background: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    margin-top: 68px;
+    align-items: center;
+    a {
+        width: 232px;
+        height: 17px;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+
+        color: #52B6FF;
+    }
+    `;
+
+const Forms = styled.form`
+    width: 303px;
+    height: 148px;
+    margin-top: 32px;
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    input {
+        width: 303px;
+        height: 45px;
+        background: #FFFFFF;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        box-sizing: border-box;
+    }
+    button {
+        width: 303px;
+        height: 45px;
+        background: #52B6FF;
+        border-radius: 5px;
+        box-sizing: border-box;
+        font-size: 20.976px;
+        line-height: 26px;
+        text-align: center;
+
+        color: #FFFFFF;
+    }
+    `;
